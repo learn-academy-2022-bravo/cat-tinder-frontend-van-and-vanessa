ab90a9f5cd727462738d37c206003fa21abb258c
Van Dexter <vanguardstellar@gmail.com>
2022-05-11 10:59:00 -0700

    done with testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Imports React into our test file.
+import React from 'react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Imports Enzyme testing and deconstructs Shallow into our test file.
+import Enzyme, { shallow } from 'enzyme'
+
+// Imports Adapter utilizing the latest react version into our test file so we can run a testing render on any component we may need.
+import Adapter from 'enzyme-adapter-react-16'
+
+// Imports in the component we are going to be testing.
+import App from './App'
+
+//Allows us to utilize the adapter we import in earlier, allowing us to call and render a component.
+Enzyme.configure({adapter: new Adapter() })
+
+describe("When App Renders", () => {
+  it("displays a Header and Footer", () => {
+      // expect something I can query to confirm my "it statement"
+      const renderedApp = shallow(<App/>)
+
+      const renderedHeader = renderedApp.find("Header")
+      const renderedFooter = renderedApp.find("Footer")
+
+
+      expect(renderedHeader.length).toEqual(1)
+      expect(renderedFooter.length).toEqual(1)
+  })
+it("provides a route of '/' to the home component", () =>{
+  const renderedApp = shallow(<App />)
+
+  const renderedHomeRoute = renderedApp.find("[path='/']")
+
+expect(renderedHomeRoute.length).toEqual(1)
+
+  })
+})
